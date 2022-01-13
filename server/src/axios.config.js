45d2672d078c4940d86f3c686b03e9047e1a7883
Sumit Dey <sumit@betterhalf.ai>
2022-01-13 23:32:19 +0530

    added express server to handle the tweet api

---
+++ server/src/axios.config.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+//! This token should be secured
+const token =
+  "AAAAAAAAAAAAAAAAAAAAAN%2FrTwEAAAAAiNdiHy38D2wQVDtjmm%2FtqpCq8os%3Df0xdoxTFWi848gAFvGnVaJfEwfKcJ5bRjc4TqigCqGY9QrMMhW";
+
+const tweeterClient = axios.create({
+  baseURL: "https://api.twitter.com/2/",
+  headers: {
+    Authorization: `Bearer ${token}`,
+  },
+});
+
+export default tweeterClient;
